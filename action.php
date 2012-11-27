7f2adf6b1d57806494cf0ca6bdeb18c96b30063d
tamtakoe <tamtakoe@gmail.com>
2012-11-27 10:28:55 +0300

    Update action.php

--- action.php
+++ action.php
@@ -9,7 +9,7 @@ mysql_query("SET NAMES 'UTF8'");
 
 //Создаем и инициализируем экземпляр класса для работы с файлами
 $album = new Album;
-$album->init();
+$album->init($param);
 
 /**
 * Класс для работы с файлами
@@ -17,19 +17,23 @@ $album->init();
 */
 class Album {
 
-	public $errors = array();
-	public $field_name = 'my-pic'; //Ключ файла в $_FILES [нужно избавиться]
-	public $upload_dir = 'uploads/'; //Папка для хранения изображений
-	public $previews_dir = 'previews/'; //Папка для хранения превьюшек
-	public $allowed_type = array('image/jpeg'=>'jpeg','image/png'=>'png','image/gif'=>'gif'); //Допустимые типы для загрузки и расширения, которые будут добавлены к именам файлов на сервере
+	protected $errors = array();
+	protected $field_name = 'my-pic'; //Ключ файла в $_FILES [нужно избавиться]
+	protected $upload_dir = 'uploads/'; //Папка для хранения изображений
+	protected $previews_dir = 'previews/'; //Папка для хранения превьюшек
+	protected $allowed_type = array('image/jpeg'=>'jpeg','image/png'=>'png','image/gif'=>'gif'); //Допустимые типы для загрузки и расширения, которые будут добавлены к именам файлов на сервере
 	//Имя файла для Blob-данных может различаться в разных браузерах, поэтому расширения имен присваиваются в соответствие с MIME типом
 	//IE image/pjpeg», «image/x-png»
+	
+	public function __constuct($type, $param) {
+		
+	}
 
-	public function init()
+	public function init($param)
 	{
 		$errors = array();
 		
-		switch ($_GET['type']) {
+		switch ($param['type']) {
 			case add:
 				//Загружаем файл на сервер
 				$file_name = $this->savefile();
@@ -57,17 +61,17 @@ class Album {
 							
 			case delete:
 				if (isset($_GET['id'])) {
-					$res = mysql_query("select `file`, `preview` from `album` where `id` = '".$_GET['id']."'");
+					$res = mysql_query("select `file`, `preview` from `album` where `id` = '".$param['id']."'");
 					$row = mysql_fetch_assoc($res);
 					unlink($this->upload_dir . $row['file']);
 					unlink($this->previews_dir . $row['preview']);
-					mysql_query("delete from `album` where `id` = '".$_GET['id']."'");					
+					mysql_query("delete from `album` where `id` = '".$param['id']."'");					
 				}
 				break;
 				
 			case update:
-				if (isset($_GET['id'])) {
-					mysql_query("update `album` set `title`='".$_POST['title']."' where `id` = '".$_GET['id']."'");				
+				if (isset($param['id'])) {
+					mysql_query("update `album` set `title`='".$param['title']."' where `id` = '".$param['id']."'");				
 				}
 				break;
 				
@@ -92,78 +96,75 @@ class Album {
 		}
 	}
 
-	public function savefile() {
-	
-		if(!empty($_FILES)) {
-			// Файл передан через обычный массив $_FILES
-			//echo 'Contents of $_FILES:<br/><pre>'.print_r($_FILES, true).'</pre>';
-			$file = $_FILES[$this->field_name];
-			
-			if ($file['error']) {			
-				$this->errors['file'] = $file['error']; //Загружен с ошибкой...
-				return false;				
-			}
-			if (!array_key_exists($file['type'], $this->allowed_type)){		
-				$this->errors['file'] = 10; //Файл неразрешенного типа
-				return false;	
-			} 	
-			
-			$new_file_name = uniqid() . '.' . $this->allowed_type[$file['type']];
+		public function savefile() {
+		try {
+			if(!empty($_FILES)) {
+				// Файл передан через обычный массив $_FILES
+				echo 'Contents of $_FILES:<br/><pre>'.print_r($_FILES, true).'</pre>';
+				$file = $_FILES[$this->field_name];
 
-			if (!move_uploaded_file($file['tmp_name'], $this->upload_dir . $new_file_name)) {
-				$this->errors['file'] = 9; //Не удалось переместить из временной директории
-				return false;
-			}
+				if ($file['error']) {			
+					throw new Exception('Загружен с ошибкой...', $file['error']);
+				}
+				if (!array_key_exists($file['type'], $this->allowed_type)){		
+					throw new Exception('Файл неразрешенного типа', 10);
+				}
+				if ($file['size'] > $this->max_file_size){	
+					throw new Exception('Превышен допустимый размер файла', 12);
+				}
+				
+				$new_file_name = uniqid() . '.' . $this->allowed_type[$file['type']];
+				if (!move_uploaded_file($file['tmp_name'], $this->upload_dir . $new_file_name)) {
+					throw new Exception('Не удалось переместить из временной директории', 9);
+				}
+				
+				return array($new_file_name, $file['size']);
 
-			return $new_file_name;	
+			} else {		
 
-		} else {		
-
-			$headers = getallheaders();
-			
-			if($headers['Content-Length'] > MAX_FILE_SIZE) {
-				$this->errors['file'] = 5; //Превышен размер файла
-				return false;
+				$headers = getallheaders();
+				
+				if($headers['Content-Length'] > $this->max_file_size) {
+					throw new Exception('Превышен размер файла, установленный на сервере', 5);
+				}
+				
+				throw new Exception('Старые браузеры пока не поддерживаются', 11);
+				
+				// Надо выцеплять файл из входного потока php
+				// (такое встречается только в очень экзотических браузерах,
+				//  поэтому можно не предусматривать этот способ вовсе)
+				if(array_key_exists('Upload-Filename', $headers)) {
+					$data = file_get_contents('php://input');
+					echo 'File recieved: '.$headers['Upload-Filename'];
+					echo '<br/>Size: '.$headers['Upload-Size'].' ('.strlen($data).' b)';
+					echo '<br/>Type: '.$headers['Upload-Type'];
+				}
 			}
+		} catch(Exception $e) {
 			
-			$this->errors['file'] = 11; //Старые браузеры пока не поддерживаются
-			return false;
-			
-			// Надо выцеплять файл из входного потока php
-			// (такое встречается только в очень экзотических браузерах,
-			//  поэтому можно не предусматривать этот способ вовсе)
-			if(array_key_exists('Upload-Filename', $headers)) {
-				$data = file_get_contents('php://input');
-				echo 'File recieved: '.$headers['Upload-Filename'];
-				echo '<br/>Size: '.$headers['Upload-Size'].' ('.strlen($data).' b)';
-				echo '<br/>Type: '.$headers['Upload-Type'];
+			switch ($e->getCode()) {		
+				case 1: //Размер принятого файла превысил максимально допустимый размер, который задан директивой upload_max_filesize конфигурационного файла php.ini
+				case 2: //Размер загружаемого файла превысил значение MAX_FILE_SIZE, указанное в HTML-форме.
+				case 5: //Размер загружаемого файла превысил все мыслимые значения.
+				case 12: //Размер загружаемого файла превысил допустимое значение.
+					$error_report = 'Файл ' . $_FILES['name'] . ' превышает разрешенный для загрузки объем ' . round($this->max_file_size/1048576, 2) . ' Мб';
+					break;
+				case 3: //Загружаемый файл был получен только частично.
+				case 4: //Файл не был загружен.
+				case 6: //Отсутствует временная папка. Добавлено в PHP 4.3.10 и PHP 5.0.3.
+				case 7: //Не удалось записать файл на диск. Добавлено в PHP 5.1.0.
+				case 8: //PHP-расширение остановило загрузку файла.
+				case 9: //Не удалось переместить из временной директории
+					$error_report = 'Загрузка не удалась';
+					break;
+				case 10: $error_report = 'Файл неразрешенного типа'; //Файл неразрешенного типа
+					break;
+				case 11: $error_report = 'Обновите браузер'; //Старые браузеры пока не поддерживаются
 			}
-		}
-	}
-	
-	public function error() {
-	
-		$error_report = '';
-		switch ($this->errors['file']) {		
-			case 1: //Размер принятого файла превысил максимально допустимый размер, который задан директивой upload_max_filesize конфигурационного файла php.ini
-			case 2: //Размер загружаемого файла превысил значение MAX_FILE_SIZE, указанное в HTML-форме.
-			case 5: //Размер загружаемого файла превысил все мыслимые значения.
-				$error_report = 'Файл ' . $_FILES['name'] . ' слишком большой. Размер загружаемого файла не должен превышать ' . round(MAX_FILE_SIZE/1048576, 2) . ' Мб';
-				break;
-			case 3: //Загружаемый файл был получен только частично.
-			case 4: //Файл не был загружен.
-			case 6: //Отсутствует временная папка. Добавлено в PHP 4.3.10 и PHP 5.0.3.
-			case 7: //Не удалось записать файл на диск. Добавлено в PHP 5.1.0.
-			case 8: //PHP-расширение остановило загрузку файла.
-			case 9: //Не удалось переместить из временной директории
-				$error_report = 'Загрузка не удалась';
-				break;
-			case 10: $error_report = 'Файл неразрешенного типа'; //Файл неразрешенного типа
-				break;
-			case 11: $error_report = 'Обновите браузер'; //Старые браузеры пока не поддерживаются
 		}
 		
 		die($error_report);
+		//return false;
 	}
 }
 
